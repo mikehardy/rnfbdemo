11650ca74f816df83c13c1911a5901a51d5258ad
Mike Hardy <github@mikehardy.net>
2019-07-26 13:55:04 -0400

    feat: show more clearly which packages are installed

--- App.js
+++ App.js
@@ -27,6 +27,21 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>React Native Firebase Demo</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text>The following modules are installed natively and working:</Text>
+        {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
+        {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
+        {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
+        {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
+        {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
+        {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
+        {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
+        {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
+        {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
+        {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
+        {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
+        {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
+        {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
+        {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
       </View>
     );
   }
