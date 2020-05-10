9894256b3cc5d41a7feef0e10a18e497cbeeca36
Mike Hardy <github@mikehardy.net>
2020-05-10 09:31:27 -0500

    RN59 copies in PodfileRN59 pre-edited, no need for CocoaPods CDN substitution
    And especially not in an incorrect place

--- make-demo-rn59.sh
+++ make-demo-rn59.sh
@@ -8,10 +8,6 @@ echo "Testing react-native 0.59 + react-native-firebase v5.current + Firebase SD
 npx react-native-cli@^1 init rnfbdemo --version="0.59.10"
 cd rnfbdemo
 
-# I have problems in my country with the cocoapods CDN sometimes, use github directly
-sed -i -e $'s/def add_flipper_pods/source \'https:\/\/github.com\/CocoaPods\/Specs.git\'\\\n\\\ndef add_flipper_pods/' ios/Podfile
-rm -f ios/Podfile.??
-
 echo "Adding react-native-firebase dependency"
 yarn add react-native-firebase
 
