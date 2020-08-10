cadef88132da674c4ccd331bd3b559c392c3f478
Mike Hardy <github@mikehardy.net>
2020-08-09 21:19:55 -0500

    Bump upstream SDK versions

--- make-demo-v6.sh
+++ make-demo-v6.sh
@@ -31,8 +31,8 @@ sed -i -e $'s/apply plugin: "com.android.application"/apply plugin: "com.android
 rm -f android/app/build.gradle??
 
 # Allow explicit SDK version control by specifying our iOS Pods and Android Firebase Bill of Materials
-echo "project.ext{set('react-native',[versions:[firebase:[bom:'25.6.0'],],])}" >> android/build.gradle
-sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'6.28.1\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
+echo "project.ext{set('react-native',[versions:[firebase:[bom:'25.7.0'],],])}" >> android/build.gradle
+sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'6.29.0\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
 rm -f ios/Podfile??
 
 # This is a reference to a pre-built version of Firestore. It's a neat trick to speed up builds.
