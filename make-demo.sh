34e0ee60972fefef769a9532b67497133a781de9
Mike Hardy <github@mikehardy.net>
2020-08-25 09:54:29 -0500

    Bump firebase-ios-sdk version, no user-visible change

--- make-demo.sh
+++ make-demo.sh
@@ -29,7 +29,7 @@ rm -f android/app/build.gradle??
 # Allow explicit SDK version control by specifying our iOS Pods and Android Firebase Bill of Materials
 echo "Adding upstream SDK overrides for precise version control"
 echo "project.ext{set('react-native',[versions:[firebase:[bom:'25.7.0'],],])}" >> android/build.gradle
-sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'6.29.0\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
+sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'6.30.0\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
 rm -f ios/Podfile??
 
 
