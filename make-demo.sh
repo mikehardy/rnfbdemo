ef9893ea710cdb7bc6da4df0e11b3f119d4e4f93
Mike Hardy <github@mikehardy.net>
2021-07-30 18:17:18 -0500

    feat: qualify react-native 0.65 (.0-rc.3), at least on intel macs
    Need to run it through on Apple Silicon but seems to work great

--- make-demo.sh
+++ make-demo.sh
@@ -6,14 +6,8 @@ set -e
 
 echo "Testing react-native current + react-native-firebase current + Firebase SDKs current"
 
-npx react-native init rnfbdemo
+npx react-native init rnfbdemo --version=0.65.0-rc.3
 cd rnfbdemo
-
-# Flipper is not needed for a demo and causes compile errors sometimes, disable it
-sed -i -e $'s/use_flipper/#use_flipper/' ios/Podfile
-rm -f ios/Podfile.??
-sed -i -e $'s/flipper_post_install/#flipper_post_install/' ios/Podfile
-rm -f ios/Podfile.??
 
 # This is the most basic integration
 echo "Adding react-native-firebase core app package"
@@ -94,10 +88,6 @@ rm -f android/build.gradle??
 sed -i -e $'s/"com.google.gms.google-services"/"com.google.gms.google-services"\\\napply plugin: "com.google.firebase.firebase-perf"/' android/app/build.gradle
 rm -f android/app/build.gradle??
 
-# Move gradle from react-native 0.64.x default to current stable on that minor version
-sed -i -e $'s/com.android.tools.build:gradle:4.1.0/com.android.tools.build:gradle:4.1.3/' android/build.gradle
-rm -f android/build.gradle??
-
 # I'm not going to demonstrate messaging and notifications. Everyone gets it wrong because it's hard. 
 # You've got to read the docs and test *EVERYTHING* one feature at a time.
 # But you have to do a *lot* of work in the AndroidManifest.xml, and make sure your MainActivity *is* the launch intent receiver
@@ -114,6 +104,12 @@ rm ./App.js && cp ../App.js ./App.js
 echo "Increasing memory available to gradle for android java build"
 echo "org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" >> android/gradle.properties
 
+# Hermes is available on both platforms and provides faster startup since it pre-parses javascript. Enable it.
+sed -i -e $'s/enableHermes: false/enableHermes: true/' android/app/build.gradle
+rm -f android/app/build.gradle??
+sed -i -e $'s/hermes_enabled => false/hermes_enabled => true/' ios/Podfile
+rm -f ios/Podfile??
+
 # In case we have any patches
 echo "Running any patches necessary to compile successfully"
 cp -rv ../patches .
@@ -123,10 +119,31 @@ npx patch-package
 if [ "$(uname)" == "Darwin" ]; then
   echo "Installing pods and running iOS app"
   cd ios && pod install --repo-update && cd ..
+
+  npx react-native run-ios || true
+
+  # For some reason (codegen related), I had to run this a second time after first build fails for it to work?
+  cd ios && pod install && cd ..
+
+  # Check iOS debug mode compile
   npx react-native run-ios
+
+  # Check iOS release mode compile
+  npx react-native run-ios --configuration "Release"
+
+  #################################
+  # Check static frameworks compile
 
   # This is how you configure for static frameworks:
   sed -i -e $'s/config = use_native_modules!/config = use_native_modules!\\\n  config = use_frameworks!\\\n  $RNFirebaseAsStaticFramework = true/' ios/Podfile
+
+  # Static frameworks does not work with hermes and flipper - toggle them both off again
+  sed -i -e $'s/use_flipper/#use_flipper/' ios/Podfile
+  rm -f ios/Podfile.??
+  sed -i -e $'s/flipper_post_install/#flipper_post_install/' ios/Podfile
+  rm -f ios/Podfile.??
+  sed -i -e $'s/hermes_enabled => true/hermes_enabled => false/' ios/Podfile
+  rm -f ios/Podfile??
 
   # Workaround needed for static framework build only, regular build is fine.
   # https://github.com/facebook/react-native/issues/31149#issuecomment-800841668
@@ -134,6 +151,9 @@ if [ "$(uname)" == "Darwin" ]; then
   rm -f ios/Podfile.??
   cd ios && pod install && cd ..
   npx react-native run-ios
+
+  # end of static frameworks workarounds + test
+  #############################################
 
   # workaround for poorly setup Android SDK environments
   USER=$(whoami)
