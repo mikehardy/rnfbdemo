9353b3a502e4d1c914aef713a3cc9491000b3259
Mike Hardy <github@mikehardy.net>
2021-11-04 11:05:53 -0500

    feat(sdks): bump firebase SDKs to ios 8.9.1 / android 29.0.0
    includes a bump to crashlytics plugin

--- make-demo.sh
+++ make-demo.sh
@@ -100,7 +100,7 @@ yarn add \
 # Crashlytics - repo, classpath, plugin, dependency, import, init
 echo "Setting up Crashlytics - package, gradle plugin"
 yarn add "@react-native-firebase/crashlytics"
-sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.firebase:firebase-crashlytics-gradle:2.7.1"/' android/build.gradle
+sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.firebase:firebase-crashlytics-gradle:2.8.0"/' android/build.gradle
 rm -f android/build.gradle??
 sed -i -e $'s/"com.google.gms.google-services"/"com.google.gms.google-services"\\\napply plugin: "com.google.firebase.crashlytics"/' android/app/build.gradle
 rm -f android/app/build.gradle??
