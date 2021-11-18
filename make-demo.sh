84deabb978e7d876e0ec321dec23daf282b6004e
Mike Hardy <github@mikehardy.net>
2021-11-18 08:19:02 -0500

    build(deps): always use the current stable react-native by default

--- make-demo.sh
+++ make-demo.sh
@@ -14,7 +14,7 @@ if ! which yarn > /dev/null 2>&1; then
   exit 1
 fi
 
-npx react-native init rnfbdemo --version=0.66.0
+npx react-native init rnfbdemo
 cd rnfbdemo
 
 # This is the most basic integration
