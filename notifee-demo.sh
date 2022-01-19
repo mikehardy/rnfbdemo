c02fc24e828cd8b1109a8115d94d74615b77f7b8
Mike Hardy <github@mikehardy.net>
2022-01-19 10:44:35 -0500

    fix(notifee-demo): notifee requires android 12 now

--- notifee-demo.sh
+++ notifee-demo.sh
@@ -8,6 +8,15 @@ echo "Testing react-native current + notifee current"
 npm_config_yes=true npx react-native init notifeedemo --version=0.66.4
 cd notifeedemo
 
+# Notifee requires Android12, so let's bump up our compile and target versions on android
+# Remove this if you use react-native 0.67+ (current as of 20220119)
+sed -i -e $'s/compileSdkVersion = 30/compileSdkVersion = 31/' android/build.gradle
+sed -i -e $'s/targetSdkVersion = 30/targetSdkVersion = 31/' android/build.gradle
+rm -f android/build.gradle??
+# Android 12 does require a tweak to the stock template AndroidManifest for compliance
+sed -i -e $'s/android:launchMode/android:exported="true"\\\n        android:launchMode/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+
 # This is the most basic integration - adding the package, adding the necessary Android local repository
 echo "Adding Notifee app package"
 yarn add "@notifee/react-native"
