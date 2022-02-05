9b549c101e5aa7645e11b612b7b264e403e60c2f
Mike Hardy <github@mikehardy.net>
2022-02-05 11:55:22 -0500

    build(deps): update to react-native 0.68.0-rc.1
    note that ios static frameworks compile is not working for unknown reason at the moment
    needs triage

--- make-demo.sh
+++ make-demo.sh
@@ -14,7 +14,7 @@ if ! which yarn > /dev/null 2>&1; then
   exit 1
 fi
 
-npm_config_yes=true npx react-native init rnfbdemo --skip-install
+npm_config_yes=true npx react-native init rnfbdemo --skip-install --version=0.68.0-rc.1
 cd rnfbdemo
 
 # New versions of react-native include a Gemfile, and it specifies a concrete ruby version. Alter it to >=
@@ -30,10 +30,10 @@ pushd ios && pod install && popd
 echo "Adding react-native-firebase core app package"
 yarn add "@react-native-firebase/app"
 echo "Adding basic iOS integration - AppDelegate import and config call"
-sed -i -e $'s/AppDelegate.h"/AppDelegate.h"\\\n@import Firebase;/' ios/rnfbdemo/AppDelegate.m
-rm -f ios/rnfbdemo/AppDelegate.m??
-sed -i -e $'s/RCTBridge \*bridge/if ([FIRApp defaultApp] == nil) { [FIRApp configure]; }\\\n  RCTBridge \*bridge/' ios/rnfbdemo/AppDelegate.m
-rm -f ios/rnfbdemo/AppDelegate.m??
+sed -i -e $'s/AppDelegate.h"/AppDelegate.h"\\\n#import <Firebase.h>/' ios/rnfbdemo/AppDelegate.mm
+rm -f ios/rnfbdemo/AppDelegate.mm??
+sed -i -e $'s/RCTBridge \*bridge/if ([FIRApp defaultApp] == nil) { [FIRApp configure]; }\\\n  RCTBridge \*bridge/' ios/rnfbdemo/AppDelegate.mm
+rm -f ios/rnfbdemo/AppDelegate.mm??
 echo "Adding basic java integration - gradle plugin dependency and call"
 sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.gms:google-services:4.3.10"/' android/build.gradle
 rm -f android/build.gradle??
@@ -42,8 +42,8 @@ rm -f android/app/build.gradle??
 
 # Allow explicit SDK version control by specifying our iOS Pods and Android Firebase Bill of Materials
 echo "Adding upstream SDK overrides for precise version control"
-echo "project.ext{set('react-native',[versions:[firebase:[bom:'29.0.2'],],])}" >> android/build.gradle
-sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'8.10.0\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
+echo "project.ext{set('react-native',[versions:[firebase:[bom:'29.0.4'],],])}" >> android/build.gradle
+sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseSDKVersion = \'8.11.0\'\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
 rm -f ios/Podfile??
 
 # This is a reference to a pre-built version of Firestore. It's a neat trick to speed up builds.
@@ -81,7 +81,7 @@ fi
 # 2.  open the .xcworkspace created by running the script to this point
 # 3.  alter the bundleID to com.rnfbdemo
 # 4.  alter the target to iPhone and iPad instead of iPhone only (Mac is not supported yet, but feel free to try...)
-# 5.  right-click on rnfbdemo, "add files to rnfbdemo" select rnfbdemo/GoogleService-Info.plist for rnfbdemo and rnfbdemo-tvOS
+# 5.  right-click on rnfbdemo, "add files to rnfbdemo" select rnfbdemo/GoogleService-Info.plist for rnfbdemo (and any other targets...)
 # 6.  copy the rnfbdemo.xcodeproj and rnfbdemo.xcworkspace folders over the existing ones saved in the root directory
 #exit 1
 rm -rf ios/rnfbdemo.xcodeproj ios/rnfbdemo.xcworkspace
@@ -90,11 +90,10 @@ cp -r ../rnfbdemo.xcworkspace ios/
 
 # From this point on we are adding optional modules
 # First set up all the modules that need no further config for the demo 
-echo "Adding packages: Analytics, App Check, App Distribution, Auth, Database, Dynamic Links, Firestore, Functions, In App Messaging, Installations, Messaging, ML, Remote Config, Storage"
+echo "Adding packages: Analytics, App Check, Auth, Database, Dynamic Links, Firestore, Functions, In App Messaging, Installations, Messaging, ML, Remote Config, Storage"
 yarn add \
   @react-native-firebase/analytics \
   @react-native-firebase/app-check \
-  @react-native-firebase/app-distribution \
   @react-native-firebase/auth \
   @react-native-firebase/database \
   @react-native-firebase/dynamic-links \
@@ -118,10 +117,16 @@ rm -f android/app/build.gradle??
 echo "Setting up Performance - package, gradle plugin"
 yarn add "@react-native-firebase/perf"
 rm -f android/app/build.gradle??
-sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.firebase:perf-plugin:1.4.0"/' android/build.gradle
+sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.firebase:perf-plugin:1.4.1"/' android/build.gradle
 rm -f android/build.gradle??
 sed -i -e $'s/"com.google.gms.google-services"/"com.google.gms.google-services"\\\napply plugin: "com.google.firebase.firebase-perf"/' android/app/build.gradle
 rm -f android/app/build.gradle??
+
+# App Distribution - classpath, plugin, dependency, import, init
+echo "Setting up Crashlytics - package, gradle plugin"
+yarn add "@react-native-firebase/app-distribution"
+sed -i -e $'s/dependencies {/dependencies {\\\n        classpath "com.google.firebase:firebase-appdistribution-gradle:3.0.0"/' android/build.gradle
+rm -f android/build.gradle??
 
 # I'm not going to demonstrate messaging and notifications. Everyone gets it wrong because it's hard. 
 # You've got to read the docs and test *EVERYTHING* one feature at a time.
@@ -144,14 +149,6 @@ sed -i -e $'s/enableHermes: false/enableHermes: true/' android/app/build.gradle
 rm -f android/app/build.gradle??
 sed -i -e $'s/hermes_enabled => false/hermes_enabled => true/' ios/Podfile
 rm -f ios/Podfile??
-
-# We want to test Android12 compatibility, so let's bump up our compile and target versions on android:
-sed -i -e $'s/compileSdkVersion = 30/compileSdkVersion = 31/' android/build.gradle
-sed -i -e $'s/targetSdkVersion = 30/targetSdkVersion = 31/' android/build.gradle
-rm -f android/build.gradle??
-# Android 12 does require a tweak to the stock template AndroidManifest for compliance
-sed -i -e $'s/android:launchMode/android:exported="true"\\\n        android:launchMode/' android/app/src/main/AndroidManifest.xml
-rm -f android/app/src/main/AndroidManifest.xml??
 
 # Apple builds in general have a problem with architectures on Apple Silicon and Intel, and doing some exclusions should help
 sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n    \\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end/' ios/Podfile
@@ -183,24 +180,26 @@ if [ "$(uname)" == "Darwin" ]; then
 
   #################################
   # Check static frameworks compile
+  # FIXME react-native 0.68.0-rc.1 - this is not working! Fails with xcodebuild error code 65, suspect find-node.sh script fail? Needs triage.
 
   # This is how you configure for static frameworks:
-  sed -i -e $'s/config = use_native_modules!/config = use_native_modules!\\\n  config = use_frameworks!\\\n  $RNFirebaseAsStaticFramework = true/' ios/Podfile
+  # sed -i -e $'s/config = use_native_modules!/config = use_native_modules!\\\n  config = use_frameworks!\\\n  $RNFirebaseAsStaticFramework = true/' ios/Podfile
 
   # Static frameworks does not work with hermes and flipper - toggle them both off again
-  sed -i -e $'s/use_flipper/#use_flipper/' ios/Podfile
-  rm -f ios/Podfile.??
-  sed -i -e $'s/flipper_post_install/#flipper_post_install/' ios/Podfile
-  rm -f ios/Podfile.??
-  sed -i -e $'s/hermes_enabled => true/hermes_enabled => false/' ios/Podfile
-  rm -f ios/Podfile??
+  # sed -i -e $'s/use_flipper/#use_flipper/' ios/Podfile
+  # rm -f ios/Podfile.??
+  # sed -i -e $'s/flipper_post_install/#flipper_post_install/' ios/Podfile
+  # rm -f ios/Podfile.??
+  # sed -i -e $'s/hermes_enabled => true/hermes_enabled => false/' ios/Podfile
+  # rm -f ios/Podfile??
 
   # Workaround needed for static framework build only, regular build is fine.
   # https://github.com/facebook/react-native/issues/31149#issuecomment-800841668
-  sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n    installer.pods_project.targets.each do |target|\\\n      if (target.name.eql?(\'FBReactNativeSpec\'))\\\n        target.build_phases.each do |build_phase|\\\n          if (build_phase.respond_to?(:name) \&\& build_phase.name.eql?(\'[CP-User] Generate Specs\'))\\\n            target.build_phases.move(build_phase, 0)\\\n          end\\\n        end\\\n      end\\\n    end/' ios/Podfile
-  rm -f ios/Podfile.??
-  cd ios && pod install && cd ..
-  npx react-native run-ios
+  # sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n    installer.pods_project.targets.each do |target|\\\n      if (target.name.eql?(\'FBReactNativeSpec\'))\\\n        target.build_phases.each do |build_phase|\\\n          if (build_phase.respond_to?(:name) \&\& build_phase.name.eql?(\'[CP-User] Generate Specs\'))\\\n            target.build_phases.move(build_phase, 0)\\\n          end\\\n        end\\\n      end\\\n    end/' ios/Podfile
+  # rm -f ios/Podfile.??
+  # cd ios && pod install && cd ..
+
+  # npx react-native run-ios
 
   # end of static frameworks workarounds + test
   #############################################
