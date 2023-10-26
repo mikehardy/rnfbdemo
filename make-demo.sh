974941d17f09e8d46db535eecb8876264e45c711
Mike Hardy <github@mikehardy.net>
2023-10-26 11:34:52 -0500

    minimal repro of macCatalyst compile failure

--- make-demo.sh
+++ make-demo.sh
@@ -2,13 +2,6 @@
 set -e 
 
 RN_VER=0.73.0-rc.3
-RNFB_VER=18.5.0
-FB_IOS_VER=10.15.0
-FB_ANDROID_VER=32.4.0
-FB_GRADLE_SERVICES_VER=4.3.15
-FB_GRADLE_PERF_VER=1.4.2
-FB_GRADLE_CRASH_VER=2.9.9
-FB_GRADLE_APP_DIST_VER=4.0.0
 
 #######################################################################################################
 #######################################################################################################
@@ -43,8 +36,8 @@ if [ "$(uname)" == "Darwin" ]; then
     printf "\n\n\n\n\n**********************************\n\n\n\n"
     printf "You must set XCODE_DEVELOPMENT_TEAM environment variable to your team id to test macCatalyst"
     printf "Try running it like: XCODE_DEVELOPMENT_TEAM=2W4T2B656C ./make-demo.sh (but with your id)"
-    printf "Skipping macCatalyst test"
     printf "\n\n\n\n\n**********************************\n\n\n\n"
+    exit 1
   fi
 fi
 
@@ -54,7 +47,7 @@ fi
 # Test: Basic template create, rnfb install, link
 \rm -fr rnfbdemo
 
-echo "Testing react-native ${RN_VER} + react-native-firebase ${RNFB_VER} + firebase-ios-sdk ${FB_IOS_VER} + firebase-android-sdk ${FB_ANDROID_VER}"
+echo "Testing react-native ${RN_VER}"
 
 if ! which yarn > /dev/null 2>&1; then
   echo "This script uses yarn, please install yarn (for example \`npm i yarn -g\` and re-try"
@@ -62,12 +55,6 @@ if ! which yarn > /dev/null 2>&1; then
 fi
 #######################################################################################################
 #######################################################################################################
-
-
-
-
-# Let's test react-native-firebase integration! Here is how you do it.
-
 
 # Initialize a fresh project.
 # We say "skip-install" because we control our ruby version and cocoapods (part of install) does not like it
@@ -85,56 +72,23 @@ NO_FLIPPER=1 USE_FRAMEWORKS=static npm_config_yes=true npx pod-install
 
 # At this point we have a clean react-native project. Absolutely stock from the upstream template.
 
-# Required: This is the most basic part of the integration - include google services plugin and call to firebase init on iOS
-echo "Adding react-native-firebase core app package"
-yarn add "@react-native-firebase/app@${RNFB_VER}"
-echo "Adding basic iOS integration - AppDelegate import and config call"
-sed -i -e $'s/AppDelegate.h"/AppDelegate.h"\\\n#import <Firebase.h>/' ios/rnfbdemo/AppDelegate.m*
-rm -f ios/rnfbdemo/AppDelegate.m*-e
-sed -i -e $'s/self.moduleName/[FIRApp configure];\\\n  self.moduleName/' ios/rnfbdemo/AppDelegate.m*
-rm -f ios/rnfbdemo/AppDelegate.m*-e
-echo "Adding basic java integration - gradle plugin dependency and call"
-sed -i -e $"s/dependencies {/dependencies {\n        classpath \"com.google.gms:google-services:${FB_GRADLE_SERVICES_VER}\"/" android/build.gradle
-rm -f android/build.gradle??
-sed -i -e $'s/apply plugin: "com.android.application"/apply plugin: "com.android.application"\\\napply plugin: "com.google.gms.google-services"/' android/app/build.gradle
-rm -f android/app/build.gradle??
-
 #############################################################################################################
 # Required: Static Frameworks linkage set up in cocoapods, and various related workarounds for compatibility.
 #############################################################################################################
 
 # CLEANUP - this is controlled now via environment variables when you run pod install:
 # NO_FLIPPER=1 USE_FRAMEWORKS=static
+
+# For ease of reproduction from command line where you may forget to set the environment variables,
+# we will manually force the Podfile to static frameworks and disabled flipper
+
 # Required: turn on static frameworks with static linkage, and tell react-native-firebase that is how we are linking
-#sed -i -e $'s/config = use_native_modules!/config = use_native_modules!\\\n  use_frameworks! :linkage => :static\\\n  $RNFirebaseAsStaticFramework = true/' ios/Podfile
+sed -i -e $'s/config = use_native_modules!/config = use_native_modules!\\\n  use_frameworks! :linkage => :static\\\n  $RNFirebaseAsStaticFramework = true/' ios/Podfile
 
 # Required Workaround: Static frameworks does not work with flipper - toggle it off (follow/vote: https://github.com/facebook/flipper/issues/3861)
-#sed -i -e $'s/:flipper_configuration/# :flipper_configuration/' ios/Podfile
-#rm -f ios/Podfile.??
+sed -i -e $'s/:flipper_configuration/# :flipper_configuration/' ios/Podfile
+rm -f ios/Podfile.??
 #############################################################################################################
-
-
-# Required: copy your Firebase config files in - you must supply them, downloaded from firebase web console
-echo "For this demo to work, you must create an \`rnfbdemo\` project in your firebase console,"
-echo "then download the android json and iOS plist app definition files to the root directory"
-echo "of this repository"
-
-echo "Copying in Firebase android json and iOS plist app definition files downloaded from console"
-
-if [ "$(uname)" == "Darwin" ]; then
-  if [ -f "../GoogleService-Info.plist" ]; then
-    cp ../GoogleService-Info.plist ios/rnfbdemo/
-  else
-    echo "Unable to locate the file 'GoogleServices-Info.plist', did you create the firebase project and download the iOS file?"
-    exit 1
-  fi
-fi
-if [ -f "../google-services.json" ]; then
-  cp ../google-services.json android/app/
-else
-  echo "Unable to locate the file 'google-services.json', did you create the firebase project and download the android file?"
-  exit 1
-fi
 
 
 ##################################################################################################
@@ -149,88 +103,19 @@ if [ "$(uname)" == "Darwin" ]; then
   source virtualenv/bin/activate
   pip install pbxproj
 
-  # set PRODUCT_BUNDLE_IDENTIFIER to com.rnfbdemo
+  # set PRODUCT_BUNDLE_IDENTIFIER to com.rnfbdemo - this may be bound to development team?
+  # you may need to change it
   sed -i -e $'s/org.reactjs.native.example/com/' ios/rnfbdemo.xcodeproj/project.pbxproj
   rm -f ios/rnfbdemo.xcodeproj/project.pbxproj-e
-
-  # Add our Google Services file to the Xcode project
-  pbxproj file ios/rnfbdemo.xcodeproj rnfbdemo/GoogleService-Info.plist --target rnfbdemo
 
   # Toggle on iPad: add build flag: TARGETED_DEVICE_FAMILY = "1,2"
   pbxproj flag ios/rnfbdemo.xcodeproj --target rnfbdemo TARGETED_DEVICE_FAMILY "1,2"
 fi
 ##################################################################################################
 
-
-# From this point on we are adding optional modules. We test them all so we add them all. You only need to add what you need.
-# First set up all the modules that need no further config for the demo 
-echo "Adding packages: Analytics, App Check, Auth, Database, Dynamic Links, Firestore, Functions, In App Messaging, Installations, Messaging, ML, Remote Config, Storage"
-yarn add \
-  @react-native-firebase/analytics@${RNFB_VER} \
-  @react-native-firebase/app-check@${RNFB_VER} \
-  @react-native-firebase/auth@${RNFB_VER} \
-  @react-native-firebase/database@${RNFB_VER} \
-  @react-native-firebase/dynamic-links@${RNFB_VER} \
-  @react-native-firebase/firestore@${RNFB_VER} \
-  @react-native-firebase/functions@${RNFB_VER} \
-  @react-native-firebase/in-app-messaging@${RNFB_VER} \
-  @react-native-firebase/installations@${RNFB_VER} \
-  @react-native-firebase/messaging@${RNFB_VER} \
-  @react-native-firebase/remote-config@${RNFB_VER} \
-  @react-native-firebase/storage@${RNFB_VER}
-
-# Optional: Crashlytics - repo, classpath, plugin, dependency, import, init
-echo "Setting up Crashlytics - package, gradle plugin"
-yarn add "@react-native-firebase/crashlytics@${RNFB_VER}"
-sed -i -e $"s/dependencies {/dependencies {\n        classpath \"com.google.firebase:firebase-crashlytics-gradle:${FB_GRADLE_CRASH_VER}\"/" android/build.gradle
-rm -f android/build.gradle??
-sed -i -e $'s/"com.google.gms.google-services"/"com.google.gms.google-services"\\\napply plugin: "com.google.firebase.crashlytics"/' android/app/build.gradle
-rm -f android/app/build.gradle??
-sed -i -e $'s/proguardFiles getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"/proguardFiles getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"\\\n            firebaseCrashlytics {\\\n                nativeSymbolUploadEnabled true\\\n                unstrippedNativeLibsDir "build\/intermediates\/merged_native_libs\/release\/out\/lib"\\\n            }/' android/app/build.gradle
-rm -f android/app/build.gradle??
-
-# Optional: Performance - classpath, plugin, dependency, import, init
-echo "Setting up Performance - package, gradle plugin"
-yarn add "@react-native-firebase/perf@${RNFB_VER}"
-rm -f android/app/build.gradle??
-sed -i -e $"s/dependencies {/dependencies {\n        classpath \"com.google.firebase:perf-plugin:${FB_GRADLE_PERF_VER}\"/" android/build.gradle
-rm -f android/build.gradle??
-sed -i -e $'s/"com.google.gms.google-services"/"com.google.gms.google-services"\\\napply plugin: "com.google.firebase.firebase-perf"/' android/app/build.gradle
-rm -f android/app/build.gradle??
-
-# Optional: App Distribution - classpath, plugin, dependency, import, init
-echo "Setting up App Distribution - package, gradle plugin"
-yarn add "@react-native-firebase/app-distribution@${RNFB_VER}"
-sed -i -e $"s/dependencies {/dependencies {\n        classpath \"com.google.firebase:firebase-appdistribution-gradle:${FB_GRADLE_APP_DIST_VER}\"/" android/build.gradle
-rm -f android/build.gradle??
-
-# Required for Firestore - android build tweak - or gradle runs out of memory during the build
-echo "Increasing memory available to gradle for android java build"
-echo "org.gradle.jvmargs=-Xmx3072m -Dfile.encoding=UTF-8" >> android/gradle.properties
-
-# I'm not going to demonstrate messaging and notifications. Everyone gets it wrong because it's hard. 
-# You've got to read the docs and test *EVERYTHING* one feature at a time.
-# But you have to do a *lot* of work in the AndroidManifest.xml, and make sure your MainActivity *is* the launch intent receiver
-# I include it for compile testing only.
-
-# Optional: do you want to configure firebase behavior via firebase.json?
-echo "Creating default firebase.json (with settings that allow iOS crashlytics to report crashes even in debug mode)"
-printf "{\n  \"react-native\": {\n    \"crashlytics_disable_auto_disabler\": true,\n    \"crashlytics_debug_enabled\": true\n  }\n}" > firebase.json
-
-# Optional: allow explicit SDK version control by specifying our iOS Pods and Android Firebase Bill of Materials
-echo "Adding upstream SDK overrides for precise version control"
-echo "project.ext{set('react-native',[versions:[firebase:[bom:'${FB_ANDROID_VER}'],],])}" >> android/build.gradle
-sed -i -e $"s/  target 'rnfbdemoTests' do/  \$FirebaseSDKVersion = '${FB_IOS_VER}'\n  target 'rnfbdemoTests' do/" ios/Podfile
-rm -f ios/Podfile??
-
-# Optional: build performance - use pre-built version of Firestore - https://github.com/invertase/firestore-ios-sdk-frameworks
-# If you are using firestore and database you *may* end up with duplicate symbol build errors referencing "leveldb", the FirebaseFirestoreExcludeLeveldb boolean fixes that.
-#sed -i -e $'s/  target \'rnfbdemoTests\' do/  $FirebaseFirestoreExcludeLeveldb = true\\\n  pod \'FirebaseFirestore\', :git => \'https:\\/\\/github.com\\/invertase\\/firestore-ios-sdk-frameworks.git\', :tag => $FirebaseSDKVersion\\\n  target \'rnfbdemoTests\' do/' ios/Podfile
-#rm -f ios/Podfile??
-
 # Optional: Apple M1 workaround - builds may have a problem with architectures on Apple Silicon and Intel, some exclusions may help
-sed -i -e $'s/post_install do |installer|/post_install do |installer|\\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end\\\n/' ios/Podfile
-rm -f ios/Podfile.??
+# sed -i -e $'s/post_install do |installer|/post_install do |installer|\\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end\\\n/' ios/Podfile
+# rm -f ios/Podfile.??
 
 # Optional: build performance optimization to use ccache - asks xcodebuild to use clang and clang++ without the fully-qualified path
 # That means that you can then make a symlink in your path with clang or clang++ and have it use a different binary
@@ -242,10 +127,6 @@ rm -f ios/Podfile??
 sed -i -e $'s/post_install do |installer|/post_install do |installer|\\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["GCC_WARN_INHIBIT_ALL_WARNINGS"] = "YES"\\\n      end\\\n    end\\\n/' ios/Podfile
 rm -f ios/Podfile??
 
-# Test: Copy in our demonstrator App.tsx
-echo "Copying demonstrator App.tsx"
-rm ./App.tsx && cp ../App.tsx ./App.tsx
-
 # Test: You have to re-run patch-package after yarn since it is not integrated into postinstall, so run it again
 echo "Running any patches necessary to compile successfully"
 cp -rv ../patches .
@@ -253,23 +134,6 @@ npm_config_yes=true npx patch-package
 
 # Test: Run the thing for iOS
 if [ "$(uname)" == "Darwin" ]; then
-
-  echo "Installing pods and running iOS app in debug mode"
-  NO_FLIPPER=1 USE_FRAMEWORKS=static npm_config_yes=true npx pod-install
-
-  # Check iOS debug mode compile
-  #CLEANUP? NO_FLIPPER=1 npx react-native run-ios
-  NO_FLIPPER=1 USE_FRAMEWORKS=static npx react-native run-ios --mode Debug
-
-  # Check iOS release mode compile
-  echo "Installing pods and running iOS app in release mode"
-  # Note: Flipper is disabled, but to make sure it is not included in release builds you have to set PRODUCTION=1 for release
-  # https://github.com/reactwg/react-native-releases/discussions/26#discussioncomment-3398711
-  # FIXME with RN0.72 I do not think PRODUCTION=1 is necessary anymore
-  #NO_FLIPPER=1 PRODUCTION=1 npx react-native run-ios --configuration "Release"
-  # CLEANUP2 NO_FLIPPER=1 npx react-native run-ios --mode "Release"
-  # FIXME this is not working anymore - hermes framework copy error / hermes-engine pod download / existence failure "Exit 65"
-  NO_FLIPPER=1 USE_FRAMEWORKS=static npx react-native run-ios --mode Release
 
   # Optional: Check catalyst build
   if ! [ "$XCODE_DEVELOPMENT_TEAM" == "" ]; then
@@ -301,67 +165,22 @@ if [ "$(uname)" == "Darwin" ]; then
     # For some reason, the device id returned if you use the computer name is wrong.
     # It is also wrong from ios-deploy or xcrun xctrace list devices
     # The only way I have found to get the right ID is to provide the wrong one then parse out the available one
-    #CATALYST_DESTINATION=$(xcodebuild -workspace ios/rnfbdemo.xcworkspace -configuration Debug -scheme rnfbdemo -destination id=7153382A-C92B-5798-BEA3-D82D195F25F8 2>&1|grep macOS|grep Catalyst|head -1 |cut -d':' -f5 |cut -d' ' -f1)
+    CATALYST_DESTINATION=$(xcodebuild -workspace ios/rnfbdemo.xcworkspace -configuration Debug -scheme rnfbdemo -destination id=7153382A-C92B-5798-BEA3-D82D195F25F8 2>&1|grep macOS|grep Catalyst|head -1 |cut -d':' -f5 |cut -d' ' -f1)
 
     # WIP This requires a CLI patch to the iOS platform to accept a UDID it cannot probe, and to set type to catalyst
     # CLEANUP? NO_FLIPPER=1 npx react-native run-ios --udid "$CATALYST_DESTINATION"
     # FIXME this is not working anymore - fatal error: 'React/RCTComponentViewProtocol.h' file not found
-    #NO_FLIPPER=1 USE_FRAMEWORKS=static npx react-native run-ios --udid "$CATALYST_DESTINATION" --mode Debug
+    NO_FLIPPER=1 USE_FRAMEWORKS=static npx react-native run-ios --udid "$CATALYST_DESTINATION" --mode Debug
+
+    # !! The build failure is mostly hidden, you see '"scodebuild" exited with error code `65`'
+    # you may see the actual build command used slightly higher in the output window, it will look like:
+    #
+    # > xcodebuild -workspace rnfbdemo.xcworkspace -configuration Debug -scheme rnfbdemo -destination id=00008112-000E45A20C2BC01E
+    #
+    # ...but with a different destination id to match your desktop
+    #
+    # If you run that command from `rnfbdemo/ios` you will get the actual compile output
+    # There you can see the actual error.
+
   fi
-
-  # Optiona: workaround for poorly setup Android SDK environments on macs
-  USER=$(whoami)
-  echo "sdk.dir=/Users/$USER/Library/Android/sdk" > android/local.properties
 fi
-
-# Test: make sure proguard and abi splits work
-echo "Configuring Android release build for ABI splits and code shrinking"
-# FIXME this is no longer a thing in RN72?
-# https://github.com/react-native-community/discussions-and-proposals/issues/602
-#sed -i -e $'s/def enableSeparateBuildPerCPUArchitecture = false/def enableSeparateBuildPerCPUArchitecture = true/' android/app/build.gradle
-#rm -f android/app/build.gradle??
-sed -i -e $'s/def enableProguardInReleaseBuilds = false/def enableProguardInReleaseBuilds = true/' android/app/build.gradle
-rm -f android/app/build.gradle??
-# FIXME not sure this is present anymore in rn72?
-sed -i -e $'s/universalApk false/universalApk true/' android/app/build.gradle
-rm -f android/app/build.gradle??
-
-# Test: If we are on WSL the user needs to now run it from the Windows side
-# Getting it to run from WSL is a real mess (it is possible, but not recommended)
-# So we will stop now that we've done all the installation and file editing
-if [ "$(uname -a | grep Linux | grep -c microsoft)" == "1" ]; then
-  echo "Detected Windows Subsystem for Linux. Stopping now."
-
-  # Clear out the unix-y node_modules
-  \rm -fr node_modules
-  echo "To run the app use Windows Powershell in the rnfbdemo directory with these commands:"
-  echo "npm i"
-  echo "npx react-native run-android --mode debug"
-  exit
-fi
-
-# Test: uninstall it (just in case, otherwise ABI-split-generated version codes will prevent debug from installing)
-pushd android
-./gradlew uninstallRelease
-popd
-
-# Test: Run it for Android (assumes you have an android emulator running)
-echo "Running android app in release mode"
-npx react-native run-android --mode release
-
-# Test: Let it start up, then uninstall it (otherwise ABI-split-generated version codes will prevent debug from installing)
-sleep 30
-pushd android
-./gradlew uninstallRelease
-popd
-
-# Workaround flipper crash problem until Android Marshmallow (release 6+)
-# see https://github.com/facebook/flipper/issues/3572
-sed -i -e 's/^import android.app.Application/import android.app.Application\nimport android.os.Build/' android/app/src/main/java/com/rnfbdemo/MainApplication.kt
-sed -i -e 's/^    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)/    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {\n      ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)\n    }/' android/app/src/main/java/com/rnfbdemo/MainApplication.kt
-rm -f android/app/src/main/java/com/rnfbdemo/MainApplication.kt??
-
-# Test: may or may not be commented out, depending on if have an emulator available
-# I run it manually in testing when I have one, comment if you like
-echo "Running android app in debug mode"
-npx react-native run-android --mode debug
