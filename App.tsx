c21ca8332478687c70bb2aa9a63c1cef7fefd264
Mike Hardy <github@mikehardy.net>
2025-02-18 21:55:17 -0500

    feat: app now sends silent and visible notifications
    unfortunate reformatting included also...

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   Button,
@@ -20,22 +20,36 @@ import {
 
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
-import { getApp, getApps } from '@react-native-firebase/app';
-import { getAnalytics } from '@react-native-firebase/analytics';
-import appCheck, { initializeAppCheck } from '@react-native-firebase/app-check';
-import { getAppDistribution} from '@react-native-firebase/app-distribution';
-import { connectAuthEmulator, FirebaseAuthTypes, getAuth, onAuthStateChanged, signInAnonymously, signOut } from '@react-native-firebase/auth';
-import { getCrashlytics }  from '@react-native-firebase/crashlytics';
-import { getDatabase } from '@react-native-firebase/database';
-import { getDynamicLinks } from '@react-native-firebase/dynamic-links';
-import { getFirestore } from '@react-native-firebase/firestore';
-import { getFunctions } from '@react-native-firebase/functions';
-import { getInAppMessaging } from '@react-native-firebase/in-app-messaging';
-import { getInstallations } from '@react-native-firebase/installations';
-import { getMessaging, getToken, onMessage } from '@react-native-firebase/messaging';
-import { getPerformance } from '@react-native-firebase/perf';
-import { getRemoteConfig } from '@react-native-firebase/remote-config';
-import { getStorage } from '@react-native-firebase/storage';
+import {getApp, getApps} from '@react-native-firebase/app';
+import {getAnalytics} from '@react-native-firebase/analytics';
+import appCheck, {initializeAppCheck} from '@react-native-firebase/app-check';
+import {getAppDistribution} from '@react-native-firebase/app-distribution';
+import {
+  connectAuthEmulator,
+  FirebaseAuthTypes,
+  getAuth,
+  onAuthStateChanged,
+  signInAnonymously,
+  signOut,
+} from '@react-native-firebase/auth';
+import {getCrashlytics} from '@react-native-firebase/crashlytics';
+import {getDatabase} from '@react-native-firebase/database';
+import {getDynamicLinks} from '@react-native-firebase/dynamic-links';
+import {getFirestore} from '@react-native-firebase/firestore';
+import {getFunctions} from '@react-native-firebase/functions';
+import {getInAppMessaging} from '@react-native-firebase/in-app-messaging';
+import {getInstallations} from '@react-native-firebase/installations';
+import {
+  getMessaging,
+  getToken,
+  isDeviceRegisteredForRemoteMessages,
+  onMessage,
+  registerDeviceForRemoteMessages,
+  requestPermission,
+} from '@react-native-firebase/messaging';
+import {getPerformance} from '@react-native-firebase/perf';
+import {getRemoteConfig} from '@react-native-firebase/remote-config';
+import {getStorage} from '@react-native-firebase/storage';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -67,9 +81,17 @@ function Section({children, title}: SectionProps): JSX.Element {
   );
 }
 
-onMessage(getMessaging(), (message) => {
+onMessage(getMessaging(), message => {
   console.log('messaging.onMessage received: ' + JSON.stringify(message));
-})
+});
+
+connectAuthEmulator(getAuth(), 'http://localhost:9099');
+
+onAuthStateChanged(getAuth(), (user: FirebaseAuthTypes.User) => {
+  console.log(
+    'onAuthStateChanged was called with user uid ' + (user?.uid ?? '(no user)'),
+  );
+});
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -92,10 +114,27 @@ function App(): JSX.Element {
         siteKey: 'unknown',
       },
     });
-    initializeAppCheck(getApp(), { provider: rnfbProvider}).then(() => {
+    initializeAppCheck(getApp(), {provider: rnfbProvider}).then(() => {
       console.log('AppCheck is initialized.');
       setAppCheckPresent(true);
     });
+
+    console.log('Requesting basic notification permission');
+    requestPermission(getMessaging(), {alert: true, badge: true}).then(() =>
+      console.log('Permission for notifications handled'),
+    );
+
+    console.log('Initializing messaging for notifications...');
+    registerDeviceForRemoteMessages(getMessaging())
+      .then(() =>
+        console.log(
+          'Registered for remote messages: ' +
+            isDeviceRegisteredForRemoteMessages(getMessaging()),
+        ),
+      )
+      .catch(e =>
+        console.error('could not register for remote notifications: ' + e),
+      );
   }, []);
 
   const backgroundStyle = {
@@ -108,47 +147,87 @@ function App(): JSX.Element {
     },
   });
 
-  connectAuthEmulator(getAuth(), 'http://localhost:9099');
-
-  onAuthStateChanged(getAuth(), (user: FirebaseAuthTypes.User) => {
-    console.log('onAuthStateChanged was called with user uid ' + user.uid);
-  });
-
   const sendSilent = async () => {
     console.log('sending a silent notification now');
+    try {
+      console.log('Getting our token for message send');
+      const token = await getToken(getMessaging());
+      console.log('sending a visible notification now');
+      const fcmRequest = await fetch(
+        'https://us-central1-react-native-firebase-testing.cloudfunctions.net/sendFCM',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            data: {
+              delay: 10000,
+              message: {
+                token,
+                data: {
+                  message: 'hello from data block',
+                },
+                apns: {
+                  payload: {
+                    aps: {
+                      'content-available': 1,
+                    },
+                  },
+                },
+                android: {
+                  priority: 'high',
+                },
+              },
+            },
+          }),
+          redirect: 'follow',
+        },
+      );
+      console.log('request sent, waiting for response');
+      const {result} = await fcmRequest.json();
+      console.log('got sendFCM result: ' + JSON.stringify(result, null, 2));
+    } catch (e) {
+      console.error('something went wrong? ' + e);
+    }
   };
 
   const sendVisible = async () => {
-    console.log('sending a visible notification now');
-    // https://sendfcm-6rg4g7hv7q-uc.a.run.app
-    const fcmRequest = await fetch(
-      'https://us-central1-react-native-firebase-testing.cloudfunctions.net/sendFCM',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          data: {
-            delay: 10000,
-            message: {
-              // TODO all the mesage stuff here
-              token: await getToken(getMessaging()),
-              notification: {
-                title: 'hello world title',
-                body: 'hello world body',
-              },
-              android:{
-                priority: 'high'
-              },
-            }
+    try {
+      console.log('Getting our token for message send');
+      const token = await getToken(getMessaging());
+      console.log('sending a visible notification now');
+      const fcmRequest = await fetch(
+        'https://us-central1-react-native-firebase-testing.cloudfunctions.net/sendFCM',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
           },
-        }),
-        redirect: 'follow',
-      },
-    );
-    const { result } = await fcmRequest.json();
-    console.log('got sendFCM result: ' + JSON.stringify(result, null, 2));
+          body: JSON.stringify({
+            data: {
+              delay: 10000,
+              message: {
+                token,
+                notification: {
+                  title: 'hello world title',
+                  body: 'hello world body',
+                },
+                data: {
+                  message: 'hello from data block',
+                },
+              },
+            },
+          }),
+          redirect: 'follow',
+        },
+      );
+      console.log('request sent, waiting for response');
+      const {result} = await fcmRequest.json();
+      console.log('got sendFCM result: ' + JSON.stringify(result, null, 2));
+    } catch (e) {
+      console.error('something went wrong? ' + e);
+    }
   };
 
   return (
@@ -160,10 +239,34 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-          <Button title="sign in" onPress={async () => { console.log('anonymous sign in '); await signInAnonymously(getAuth())}} />
-          <Button title="sign out" onPress={async () => { console.log('signing out'); await signOut(getAuth()); }} />
-          <Button title="Send Silent Notification to Device" onPress={async () => { console.log('silent notification'); await sendSilent()}} />
-          <Button title="Send Visible Notification to Device" onPress={async () => { console.log('visible notification'); await sendVisible()}} />
+        <Button
+          title="sign in"
+          onPress={async () => {
+            console.log('anonymous sign in ');
+            await signInAnonymously(getAuth());
+          }}
+        />
+        <Button
+          title="sign out"
+          onPress={async () => {
+            console.log('signing out');
+            await signOut(getAuth());
+          }}
+        />
+        <Button
+          title="Send Silent Notification to Device"
+          onPress={async () => {
+            console.log('silent notification');
+            await sendSilent();
+          }}
+        />
+        <Button
+          title="Send Visible Notification to Device"
+          onPress={async () => {
+            console.log('visible notification');
+            await sendVisible();
+          }}
+        />
 
         <View
           style={{
@@ -172,26 +275,56 @@ function App(): JSX.Element {
           }}>
           <Section title="RNFirebase Build Demo" />
           <Text />
-          <Text style={dynStyles.colors}>JSI Executor: {global.__jsiExecutorDescription}</Text>
+          <Text style={dynStyles.colors}>
+            JSI Executor: {global.__jsiExecutorDescription}
+          </Text>
           <Text />
-          <Text style={dynStyles.colors}>These firebase modules appear to be working:</Text>
+          <Text style={dynStyles.colors}>
+            These firebase modules appear to be working:
+          </Text>
           <Text />
           {getApps().length && <Text style={dynStyles.colors}>app()</Text>}
-          {getAnalytics().native && <Text style={dynStyles.colors}>analytics()</Text>}
+          {getAnalytics().native && (
+            <Text style={dynStyles.colors}>analytics()</Text>
+          )}
           {appCheckPresent && <Text style={dynStyles.colors}>appCheck()</Text>}
-          {getAppDistribution().native && <Text style={dynStyles.colors}>appDistribution()</Text>}
+          {getAppDistribution().native && (
+            <Text style={dynStyles.colors}>appDistribution()</Text>
+          )}
           {getAuth().native && <Text style={dynStyles.colors}>auth()</Text>}
-          {getCrashlytics().native && <Text style={dynStyles.colors}>crashlytics()</Text>}
-          {getDatabase().native && <Text style={dynStyles.colors}>database()</Text>}
-          {getDynamicLinks().native && <Text style={dynStyles.colors}>dynamicLinks()</Text>}
-          {getFirestore().native && <Text style={dynStyles.colors}>firestore()</Text>}
-          {getFunctions().native && <Text style={dynStyles.colors}>functions()</Text>}
-          {getInAppMessaging().native && <Text style={dynStyles.colors}>inAppMessaging()</Text>}
-          {getInstallations().native && <Text style={dynStyles.colors}>installations()</Text>}
-          {getMessaging().native && <Text style={dynStyles.colors}>messaging()</Text>}
-          {getPerformance().native && <Text style={dynStyles.colors}>perf()</Text>}
-          {getRemoteConfig().native && <Text style={dynStyles.colors}>remoteConfig()</Text>}
-          {getStorage().native && <Text style={dynStyles.colors}>storage()</Text>}
+          {getCrashlytics().native && (
+            <Text style={dynStyles.colors}>crashlytics()</Text>
+          )}
+          {getDatabase().native && (
+            <Text style={dynStyles.colors}>database()</Text>
+          )}
+          {getDynamicLinks().native && (
+            <Text style={dynStyles.colors}>dynamicLinks()</Text>
+          )}
+          {getFirestore().native && (
+            <Text style={dynStyles.colors}>firestore()</Text>
+          )}
+          {getFunctions().native && (
+            <Text style={dynStyles.colors}>functions()</Text>
+          )}
+          {getInAppMessaging().native && (
+            <Text style={dynStyles.colors}>inAppMessaging()</Text>
+          )}
+          {getInstallations().native && (
+            <Text style={dynStyles.colors}>installations()</Text>
+          )}
+          {getMessaging().native && (
+            <Text style={dynStyles.colors}>messaging()</Text>
+          )}
+          {getPerformance().native && (
+            <Text style={dynStyles.colors}>perf()</Text>
+          )}
+          {getRemoteConfig().native && (
+            <Text style={dynStyles.colors}>remoteConfig()</Text>
+          )}
+          {getStorage().native && (
+            <Text style={dynStyles.colors}>storage()</Text>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
